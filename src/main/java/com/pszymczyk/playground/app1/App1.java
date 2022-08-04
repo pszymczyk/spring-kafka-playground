5dded99cf9d39d6779686c08ef119a1e6f9f20a9
Pawel Szymczyk <pawel.szymczyk@displate.com>
2022-08-04 11:48:54 +0200

    app1

--- src/main/java/com/pszymczyk/playground/app1/App1.java
+++ src/main/java/com/pszymczyk/playground/app1/App1.java
@@ -1,4 +1,59 @@
 package com.pszymczyk.playground.app1;
 
-class App1 {
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.RoutingKafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+@SpringBootApplication
+public class App1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App1.class, args).close();
+    }
+
+    @Bean
+    public RoutingKafkaTemplate routingTemplate(GenericApplicationContext context,
+                                                ProducerFactory<Object, Object> pf) {
+
+        Map<String, Object> configs = new HashMap<>(pf.getConfigurationProperties());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        var jsonPF = new DefaultKafkaProducerFactory<>(configs);
+        context.registerBean(DefaultKafkaProducerFactory.class, "jsonProducerFactory", jsonPF);
+
+        Map<Pattern, ProducerFactory<Object, Object>> map = new LinkedHashMap<>();
+        map.put(Pattern.compile("app1-messages"), jsonPF);
+        map.put(Pattern.compile(".+"), pf);
+        return new RoutingKafkaTemplate(map);
+    }
+
+    @Bean
+    public ApplicationRunner runner(RoutingKafkaTemplate routingKafkaTemplate) {
+        return args -> {
+            routingKafkaTemplate.send("app1-messages", new Message("sender", "receiver", "Hello world!"));
+            routingKafkaTemplate.send("app1-default", "Plain text Hello World!");
+        };
+    }
+
+    @Bean
+    public NewTopic messages() {
+        return new NewTopic("app1-messages", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic defaultTopic() {
+        return new NewTopic("app1-default", 1, (short) 1);
+    }
 }
---
+++ src/main/java/com/pszymczyk/playground/app1/Message.java
@@ -0,0 +1,4 @@
+package com.pszymczyk.playground.app1;
+
+public record Message(String sender, String receiver, String value) {
+}
