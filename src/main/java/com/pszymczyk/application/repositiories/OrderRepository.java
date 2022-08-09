2a0a0bcd04af28d0d0f9c0f20767194aec0670b0
Pawel Szymczyk <pawel.szymczyk@displate.com>
2022-08-09 19:15:56 +0200

    remove application

--- src/main/java/com/pszymczyk/application/repositiories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pszymczyk.application.repositiories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface OrderRepository extends CrudRepository<OrderEntity, Long> {
-    Optional<OrderEntity> findByOrderId(String orderId);
-}
